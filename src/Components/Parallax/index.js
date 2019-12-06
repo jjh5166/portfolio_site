5000deb9210a29ad21b002ba2d39450a7b6ddb7c
John <jjh5166@gmail.com>
2019-12-05 23:05:23 -0500

    parallax styled components

---
+++ src/Components/Parallax/index.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const ParallaxGroup = styled.div`
+  position: relative;
+  height: 100vh;
+  transform-style: preserve-3d;
+  outline: solid orange 2px;
+`
+
+export const ParallaxLayer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  text-align: center;
+`
+
+export const ParallaxContainer = styled.div`
+  height: 100vh;
+  perspective: 300px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  font-size: 200%;
+  scroll-behavior: smooth;  
+`
