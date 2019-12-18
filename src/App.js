7556cc2dfeb65624931a757f151df5816b831832
John <jjh5166@gmail.com>
2019-12-17 20:26:48 -0500

    scroll refs, css onScroll, rm useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { useState } from 'react';
+import React, { useRef} from 'react';
 import $ from 'jquery';
 import {ParallaxContainer} from './Components/Parallax'
 import GroupOne from './Components/GroupOne'
 import GroupTwo from './Components/GroupTwo'
 import GroupThree from './Components/GroupThree'
-
 function App() {
-
-  const [scrollTop, setScroll] = useState(0);
+  const group2 = useRef(null);
+  // const waves = useRef(null); //ref to wave layer not set
   const debugOn = () => {
     $('.parallax__group').toggleClass('group-debug')
   };
 
   const handleScroll = (event) => {
-    setScroll(event.target.scrollTop)
+    let scroller = event.target.scrollTop
+    let opac = 'rgba(130,50,75, ' + Math.min(1, ((.002 * scroller) + .4)) + ')';
+    $('.lagos').css('background-color', opac)
     event.stopPropagation()
   }
 
   return (
-    <ParallaxContainer onScroll= {handleScroll}>
-      <button className="thebutton" onClick={debugOn}>debug</button>
-      <GroupOne scrollTop={scrollTop}/>
-      <GroupTwo/>
-      <GroupThree/>
-    </ParallaxContainer>
+
+      <ParallaxContainer onScroll= {handleScroll}>
+        <button className="thebutton" onClick={debugOn}>debug</button>
+        <GroupOne nextRef={group2}/>
+        <GroupTwo ref={group2} fordwardRef={waves}/>
+        <GroupThree/>
+      </ParallaxContainer>
+
   );
 }
 
