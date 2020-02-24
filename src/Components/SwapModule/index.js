e70c42fc406ce84977022bf07efff0422a59b800
John <jjh5166@gmail.com>
2020-02-23 22:28:50 -0500

    group3 Nav size on small mobile

--- src/Components/SwapModule/index.js
+++ src/Components/SwapModule/index.js
@@ -35,7 +35,10 @@ height: 100%;
 
 const BottomContainer = styled.div`
 width: 100%;
-height: 94%;
+height: 92%;
+@media only screen and (min-height: 600px) and (orientation: portrait){
+  height: 94%;
+}
 @media only screen and (max-width: 823px) and (orientation: landscape){
   height: 90%;
 }
